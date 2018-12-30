671bd8e8311bc6ef70a272cd2cf2a7ea7adde5ad
Joanna <lewj@oregonstate.edu>
2018-12-30 14:36:17 -0800

    debugging and routes

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 const port = process.env.PORT || 5000;
-
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 app.get("/", (req, res) => res.send("Testing mongoose."));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
